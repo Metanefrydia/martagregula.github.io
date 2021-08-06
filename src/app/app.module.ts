5e8ca052ac68c346f4b84e21022766a65bf0260c
Metanefrydia <mgregula10@gmail.com>
2021-08-06 15:40:04 +0200

    Added basic project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { WelcomeSectionComponent } from './layout/welcome-section/welcome-section.component';
+import { SkillSectionComponent } from './layout/skill-section/skill-section.component';
+import { ContactSectionComponent } from './layout/contact-section/contact-section.component';
+import { ProjectSectionComponent } from './layout/project-section/project-section.component';
+import { ProjectCardComponent } from './layout/project-section/project-card/project-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    WelcomeSectionComponent,
+    SkillSectionComponent,
+    ContactSectionComponent,
+    ProjectSectionComponent,
+    ProjectCardComponent
   ],
   imports: [
     BrowserModule,
