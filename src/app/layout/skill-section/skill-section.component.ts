038f7eeb9f3649c8fcce88c164c02503c261cbe8
Metanefrydia <mgregula10@gmail.com>
2021-08-09 21:45:48 +0200

    Added skills section

--- src/app/layout/skill-section/skill-section.component.ts
+++ src/app/layout/skill-section/skill-section.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skill-section',
   templateUrl: './skill-section.component.html',
   styleUrls: ['./skill-section.component.scss']
 })
-export class SkillSectionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SkillSectionComponent {
+  public skills = ['Angular', 'Typescript', 'HTML', 'CSS', 'React', 'Figma', 'VS Code', 'Git'];
 }
